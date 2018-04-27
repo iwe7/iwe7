42676d1bbaaad5ea7a7913fa44abd00565cf495e
iwe7 <admin@meepo.com.cn>
2018-04-27 16:45:16 +0800

    添加css后缀

--- apps/app-table/src/app/app.module.ts
+++ apps/app-table/src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { SharedModule } from 'iwe7/shared';
 import { RouterModule } from '@angular/router';
 import { NxModule } from '@nrwl/nx';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { NgrxModule } from 'iwe7/ngrx';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { storeFreeze } from 'ngrx-store-freeze';
+
+import { NgrxModule } from 'iwe7/ngrx';
+
 @NgModule({
   imports: [
     BrowserModule,
-    SharedModule,
     RouterModule.forRoot([]),
     NxModule.forRoot(),
     NgrxModule,
